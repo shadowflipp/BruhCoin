3e879a4e5d71e1e53f58ad0da51e62d825ec7c26
Carlo Gallo <carlo.gallo@bluewin.ch>
2022-11-30 09:53:15 +0100

    added integrity check for blocks

--- src/BruhCoin.java
+++ src/BruhCoin.java
@@ -1,13 +1,37 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder; 
+
 public class BruhCoin {
+	public static ArrayList<Block> bruhChain = new ArrayList<Block>();
+	
 	public static void main(String[] args) {
 		
-		Block genesisBlock = new Block("Hi im the first block", "0");
-		System.out.println("Hash for block 1: " + genesisBlock.hash);
+		bruhChain.add(new Block("Hi im the first Block", "0"));
+		bruhChain.add(new Block("Hi im the second Block", bruhChain.get(bruhChain.size()-1).hash));
+		bruhChain.add(new Block("Hi im the third Block", bruhChain.get(bruhChain.size()-1).hash));
 		
-		Block secondBlock = new Block("Hi im the second block", genesisBlock.hash);
-		System.out.println("Hash for block 2: " + secondBlock.hash);
+		String bruhChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(bruhChain);	
+		System.out.println(bruhChainJson);
+	}
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
 		
-		Block thirdBlock = new Block("Hi im the third block", secondBlock.hash);
-		System.out.println("Hash for block 3: " + thirdBlock.hash);
+		for(int i = 1; i < bruhChain.size(); i++) {
+			currentBlock = bruhChain.get(i);
+			previousBlock = bruhChain.get(i - 1);
+			
+			if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+				System.out.println("Current Hashes not equal");
+				return false;
+			}
+			
+			if(!previousBlock.hash.equals(currentBlock.previousHash)){
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			
+		}
+		return true;
 	}
 }
